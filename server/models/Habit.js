c6eef2a491cf339e47cd872827f37a52a851b017
polinamoore <p_gerasimenko@yahoo.com>
2021-07-26 12:48:55 +0100

    Update models and controllers

--- server/models/Habit.js
+++ server/models/Habit.js
@@ -22,20 +22,20 @@ class Habit {
         })
     }
 
-    static findByEmail(email){
+    static filterByEmail(email){
         return new Promise(async (res, rej) => {
             try {
-                let result = await db.query(`SELECT * FROM users
+                let result = await db.query(`SELECT * FROM habits
                                                 WHERE email = $1;`, [email]);
-                let user = new User(result.rows[0])
-                res(user)
+                let habits = result.rows.map(habit => new Habit(habit))
+                res(habits) // Returns an array of habits for a particular user
             } catch (err) {
                 rej(`Error retrieving user: ${err}`)
             }
         })
     }
 
-    static get all() {
+    static update() {
         return new Promise(async (res, rej) => {
             try {
                 let result = await db.query(`SELECT * FROM habits;`);
