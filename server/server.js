c78304642ce3fe8c912fe3c38a0642c7a4fa0ae5
polinamoore <p_gerasimenko@yahoo.com>
2021-07-26 11:35:44 +0100

    add user_auth controller

--- server/server.js
+++ server/server.js
@@ -5,10 +5,8 @@ const cors = require('cors');
 app.use(cors('*'));
 app.use(express.json());
 
-const authRoutes = require('./controllers/auth');
-const userRoutes = require('./controllers/users');
+const authRoutes = require('./controllers/user_auth');
 app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
 
 app.get('/', (req, res) => res.json({ message: 'Welcome' }));
 
