7e68ed1c739e5cdb0f37bdc6bb32d29cd0076bd9
Tom Virgo <tomcvirgo@gmail.com>
2021-07-26 17:05:27 +0100

    add event listeners for profile page

--- client/assets/js/lib/handlers.js
+++ client/assets/js/lib/handlers.js
@@ -1,29 +1,35 @@
-const { onLoginButtonClick, onRegistrationButtonClick } = require('./event_handlers/index');
+const { onLoginButtonClick, onRegistrationButtonClick } = require("./event_handlers/index");
+const { onChangePasswordSumbit, onUpdateUserInfoSumbit } = require("./event_handlers/profile");
 
 function bindIndexListeners() {
-	const loginButton = document.querySelector('.login');
-	loginButton.addEventListener('click', onLoginButtonClick);
+  const loginButton = document.querySelector(".login");
+  loginButton.addEventListener("click", onLoginButtonClick);
 
-	const registrationButton = document.querySelector('.register');
-	registrationButton.addEventListener('click', onRegistrationButtonClick);
+  const registrationButton = document.querySelector(".register");
+  registrationButton.addEventListener("click", onRegistrationButtonClick);
 }
 
 function bindDashboardListeners() {}
 
-function bindProfileListeners() {}
+function bindProfileListeners() {
+  const changeUserInfoSubmitButton = document.getElementById("user-info-form");
+  changeUserInfoSubmitButton.addEventListener("submit", onUpdateUserInfoSumbit);
+
+  const changePasswordSubmitButton = document.getElementById("change-password-form");
+  changePasswordSubmitButton.addEventListener("submit", onChangePasswordSumbit);
+}
 
 function renderHabits() {}
 
 function initPageBindings() {
-	const path = window.location.pathname;
-
-	if (path === '/') {
-		bindIndexListeners();
-	} else if (path === '/dashboard') {
-		bindDashboardListeners();
-	} else if (path === '/profile') {
-		bindProfileListeners();
-	}
+  const path = window.location.pathname;
+  if (path === "/") {
+    bindIndexListeners();
+  } else if (path === "/dashboard") {
+    bindDashboardListeners();
+  } else if (path === "/profile.html") {
+    bindProfileListeners();
+  }
 }
 
 module.exports = initPageBindings;
