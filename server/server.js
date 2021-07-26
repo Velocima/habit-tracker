925fe026ae39ef2f42c55e877640c7f9656a8bfc
polinamoore <p_gerasimenko@yahoo.com>
2021-07-26 14:58:46 +0100

    Update full crud functionality

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ app.use(cors('*'));
 app.use(express.json());
 
 const authRoutes = require('./controllers/auth');
+const habitsRoutes = require('./controllers/habits');
+const userRoutes = require('./controllers/user');
 app.use('/auth', authRoutes);
+app.use('/habits', habitsRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => res.json({ message: 'Welcome' }));
 
