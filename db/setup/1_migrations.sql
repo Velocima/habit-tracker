93941b15144351226c681fc56397686f2093b5f5
polinamoore <p_gerasimenko@yahoo.com>
2021-07-26 10:54:18 +0100

    update models and db data

--- db/setup/1_migrations.sql
+++ db/setup/1_migrations.sql
@@ -1,9 +1,16 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id serial PRIMARY KEY,
-    -- username varchar(255) NOT NULL UNIQUE, Do we need a username??
-    email varchar(100) NOT NULL UNIQUE,
-    password_digest varchar(500) NOT NULL
-    -- Add another column as aproppriate 
+    email PRIMARY KEY varchar(100) NOT NULL UNIQUE,
+    name varchar(100) NOT NULL UNIQUE,
+    password_digest varchar(500) NOT NULL UNIQUE
 );
+
+CREATE TABLE habits (
+    email PRIMARY KEY varchar(100) NOT NULL UNIQUE,
+    habit_name varchar(255) NOT NULL UNIQUE,
+    habit_description varchar(255) NOT NULL UNIQUE,
+    habit_frequency varchar(255) NOT NULL UNIQUE,
+    frequency_target int NOT NULL UNIQUE,
+    streak int NOT NULL UNIQUE
+)
--- server/models/User.js
+++ server/models/User.js
@@ -4,13 +4,13 @@ class User {
     constructor(data){
         this.email = data.email
         this.name = data.name
-        this.passwordDigest = data.password_digest
+        this.passwordDigest = data.passwordDigest
     }
 
-    static create({ name, email, password }){
+    static create({ name, email, passwordDigest }){
         return new Promise(async (res, rej) => {
             try {
-                let result = await db.query(`INSERT INTO users (name, email, password_digest) VALUES $1, $2, $3 RETURNING *;`, [name, email, password]);
+                let result = await db.query(`INSERT INTO users (name, email, password_digest) VALUES $1, $2, $3 RETURNING *;`, [name, email, passwordDigest]);
                 let user = new User(result.rows[0]);
                 res(user)
             } catch (err) {
