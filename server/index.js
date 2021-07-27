8db17f16d61ad3bf193bf210112252c76637c163
Velo <hartley.max@outlook.com>
2021-07-27 16:04:13 +0100

    config dotenv in server root

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const app = require('./server');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Express just departed from port ${port}!`))
+app.listen(port, () => console.log(`Express just departed from port ${port}!`));
