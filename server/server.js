e0bfe85e502394249e28d9d208450d1729cd25ad
Velo <hartley.max@outlook.com>
2021-07-27 15:48:53 +0100

    add morgan for logging

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 
 app.use(cors('*'));
+app.use(morgan('dev'));
 app.use(express.json());
 
 /**
