fef9bf6ea8cb8bdf3896729e2b8a7dd9bd4eaa5f
Velo <hartley.max@outlook.com>
2021-07-27 17:11:15 +0100

    update post habit url

--- client/assets/js/lib/requests.js
+++ client/assets/js/lib/requests.js
@@ -1,100 +1,101 @@
-const { logout } = require("./auth");
+const { logout } = require('./auth');
 
-const devURL = "http://localhost:3000";
+const devURL = 'http://localhost:3000';
 
 async function getAllUserHabits(email) {
-  try {
-    const options = { headers: new Headers({ Authorization: localStorage.getItem("token") }) };
-    const response = await fetch(`${devURL}/habits/${email}`, options);
-    const data = await response.json();
-    if (data.err) {
-      console.warn(data.err);
-      logout();
-    }
-    return data;
-  } catch (err) {
-    console.warn(err);
-  }
+	try {
+		const options = { headers: new Headers({ Authorization: localStorage.getItem('token') }) };
+		const response = await fetch(`${devURL}/habits/${email}`, options);
+		const data = await response.json();
+		if (data.err) {
+			console.warn(data.err);
+			// logout();
+		}
+		return data;
+	} catch (err) {
+		console.warn(err);
+	}
 }
 
 async function postHabit(data) {
-  try {
-    const options = {
-      method: "POST",
-      headers: new Headers({
-        Authorization: localStorage.getItem("token"),
-        "Content-Type": "application/json",
-      }),
-      body: JSON.stringify(data),
-    };
-
-    const response = await fetch(`${devURL}/habits`, options);
-    const responseJson = await response.json();
-    if (responseJson.err) {
-      throw new Error(err);
-    } else {
-      // add a new habit (require from dom_elements.js)
-    }
-  } catch (err) {
-    console.warn(err);
-  }
+	try {
+		const options = {
+			method: 'POST',
+			headers: new Headers({
+				Authorization: localStorage.getItem('token'),
+				'Content-Type': 'application/json',
+			}),
+			body: JSON.stringify(data),
+		};
+		const email = localStorage.getItem('email');
+		const url = `${devURL}/user/${email}/habits`;
+		const response = await fetch(url, options);
+		const responseJson = await response.json();
+		if (responseJson.err) {
+			throw new Error(err);
+		}
+		console.log(responseJson);
+		return responseJson;
+	} catch (err) {
+		console.warn(err);
+	}
 }
 
 async function deleteHabit(id) {
-  try {
-    const options = {
-      method: "DELETE",
-      headers: new Headers({ Authorization: localStorage.getItem("token") }),
-    };
-    const response = await fetch(`${devURL}/habits/${id}`, options);
-    const responseJson = await response.json();
-    if (responseJson.err) {
-      throw Error(err);
-    }
-  } catch (err) {
-    console.warn(err);
-  }
+	try {
+		const options = {
+			method: 'DELETE',
+			headers: new Headers({ Authorization: localStorage.getItem('token') }),
+		};
+		const response = await fetch(`${devURL}/habits/${id}`, options);
+		const responseJson = await response.json();
+		if (responseJson.err) {
+			throw Error(err);
+		}
+	} catch (err) {
+		console.warn(err);
+	}
 }
 
 async function putHabit(data) {
-  try {
-    const options = {
-      method: "PUT",
-      headers: new Headers({ Authorization: localStorage.getItem("token") }),
-      body: JSON.stringify(data),
-    };
-    const response = await fetch(`${devURL}/habits/${data.id}`, options);
-    const responseJson = await response.json();
-    if (responseJson.err) {
-      throw Error(err);
-    } else {
-      // redirect to the dashboard
-      console.log(responseJson);
-    }
-  } catch (err) {
-    console.warn(err);
-  }
+	try {
+		const options = {
+			method: 'PUT',
+			headers: new Headers({ Authorization: localStorage.getItem('token') }),
+			body: JSON.stringify(data),
+		};
+		const response = await fetch(`${devURL}/habits/${data.id}`, options);
+		const responseJson = await response.json();
+		if (responseJson.err) {
+			throw Error(err);
+		} else {
+			// redirect to the dashboard
+			console.log(responseJson);
+		}
+	} catch (err) {
+		console.warn(err);
+	}
 }
 
 async function putUserInfo(data) {
-  try {
-    const options = {
-      method: "PUT",
-      headers: new Headers({ Authorization: localStorage.getItem("token") }),
-      body: JSON.stringify(data),
-    };
-    const response = await fetch(`${devURL}/user/${data.email}`, options);
-    const responseJson = await response.json();
-    if (responseJson.err) {
-      throw Error(err);
-    } else {
-      // redirect to the dashboard
-      console.log(responseJson);
-    }
-  } catch (err) {
-    console.warn(err);
-  }
-  return responseJson;
+	try {
+		const options = {
+			method: 'PUT',
+			headers: new Headers({ Authorization: localStorage.getItem('token') }),
+			body: JSON.stringify(data),
+		};
+		const response = await fetch(`${devURL}/user/${data.email}`, options);
+		const responseJson = await response.json();
+		if (responseJson.err) {
+			throw Error(err);
+		} else {
+			// redirect to the dashboard
+			console.log(responseJson);
+		}
+	} catch (err) {
+		console.warn(err);
+	}
+	return responseJson;
 }
 
 module.exports = { getAllUserHabits, postHabit, deleteHabit, putHabit, putUserInfo };
