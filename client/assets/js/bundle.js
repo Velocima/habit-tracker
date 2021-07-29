8bfc59e1d0f8e959c935387fb95c2a942df241df
Velo <hartley.max@outlook.com>
2021-07-29 10:26:15 +0100

    fix infinite loop created by validating user

--- client/assets/js/bundle.js
+++ client/assets/js/bundle.js
@@ -694,8 +694,96 @@ module.exports = {
 };
 
 },{"./auth":2}],9:[function(require,module,exports){
+const { createHabit } = require('./dom_elements');
 
-},{}],10:[function(require,module,exports){
+function toggleNav() {
+	const nav = document.querySelector('nav');
+	if (nav.classList.contains('hide-nav')) {
+		nav.classList.remove('hide-nav');
+	} else {
+		nav.classList.add('hide-nav');
+	}
+}
+
+function addNewHabitToDOM(data) {
+	const habits = document.querySelector('.habits-container');
+	const habit = createHabit(data);
+	habits.insertBefore(habit, habits.firstChild);
+	return habit;
+}
+
+function addNameToDashboard() {
+	const welcomeMessage = document.getElementById('welcome');
+	welcomeMessage.textContent = `Welcome, ${localStorage.getItem('name')}`;
+}
+
+function addNameToProfileInput() {
+	const name = localStorage.getItem('name');
+	const nameInput = document.getElementById('name');
+	nameInput.setAttribute('value', name);
+}
+
+function validateUser() {
+	const path = window.location.pathname;
+	const name = localStorage.getItem('name');
+	const email = localStorage.getItem('email');
+	const token = localStorage.getItem('token');
+
+	if ((path === '/' || path === '/index.html') && (!name || !email || !token)) {
+		return;
+	}
+	if (!name || !email || !token) {
+		localStorage.clear();
+		window.location.pathname = '/';
+		return;
+	}
+	//validate token
+	if ((path === '/' || path === '/index.html') && name && email && token) {
+		window.location.pathname = '/dashboard.html';
+	}
+}
+
+// function bringUpEditModal(data) {
+// 	//Note this function does not work as it should atm..!
+// 	document.getElementById('submit-habit').remove();
+// 	const submitButton = document.createElement('input');
+// 	submitButton.setAttribute('type', 'submit');
+// 	submitButton.setAttribute('value', 'Submit');
+// 	submitButton.addEventListener('Submit', () => {
+// 		// make edit request here
+// 		const data = Object.fromEntries(new FormData(e.target));
+// 		console.log(data);
+// 		window.location.pathname = '/dashboard.html';
+// 	});
+
+// 	const habitModal = document.querySelector('.habit-modal');
+// 	habitModal.removeAttribute('style');
+
+// 	const form = document.querySelector('form');
+// 	form.append(submitButton);
+
+// 	const name = document.getElementById('name');
+// 	name.setAttribute('value', data.habitName);
+
+// 	const frequency = document.getElementById('frequency');
+// 	frequency.setAttribute('value', data.frequency);
+
+// 	const goal = document.getElementById('goal');
+// 	goal.setAttribute('value', data.frequencyTarget);
+
+// 	const modal = document.querySelector('.habit-modal');
+// 	modal.classList.remove('hidden');
+// }
+
+module.exports = {
+	toggleNav,
+	addNewHabitToDOM,
+	addNameToDashboard,
+	addNameToProfileInput,
+	validateUser,
+};
+
+},{"./dom_elements":3}],10:[function(require,module,exports){
 async function createChart(data = true) {
 	// DEFINE CHART LOCATIONS (IDS)
 	// -----------------------------
