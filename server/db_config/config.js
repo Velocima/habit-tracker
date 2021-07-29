5c5a3a7209200e994da7994e7138464096163132
Velo <hartley.max@outlook.com>
2021-07-29 21:46:47 +0100

    modify server for deployment

--- server/db_config/config.js
+++ server/db_config/config.js
@@ -1,6 +1,15 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
-const pool = new Pool();
-
-
+let pool;
+if (process.env.NODE_ENV === 'production') {
+	pool = new Pool({
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
+} else {
+	pool = new Pool();
+}
+client.connect();
 module.exports = pool;
